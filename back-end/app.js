ae718ce606db9b75d1fbbe28b64b98adaf4cb6c3
RomaOkhrimenko <roma.okhrimenko@gmail.com>
2022-10-15 09:18:14 +0300

    authorization

--- back-end/app.js
+++ back-end/app.js
@@ -1,13 +1,21 @@
+require("dotenv").config()
 const express = require('express')
 const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const errorMiddleware = require('./middlewares/error.middleware')
 
 const app = express()
 
 app.use(express.json())
-
+app.use(cookieParser())
 app.use(cors({
     credentials: true,
     origin: '*'
 }))
+
+const userRoute = require('./routes/user.route')
+app.use('/api', userRoute)
+
+app.use(errorMiddleware)
 
 module.exports = app
--- back-end/config/database.js
+++ back-end/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const connectDatabase = async () => {
-    await mongoose.connect(`mongodb+srv://social:tgrf7531@cluster0.ilgte6d.mongodb.net/?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology: true})
+    await mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
         .then(() => {
             console.log('mongoose connect')
         })
