3f2946a7f4eaf8a82a74d3e05a831efcfac625e4
RomaOkhrimenko <roma.okhrimenko@gmail.com>
2022-11-21 22:02:15 +0200

    try 4

--- front-end/src/store/redux/api/gamesApi.ts
+++ front-end/src/store/redux/api/gamesApi.ts
@@ -3,7 +3,7 @@ import {IGame} from "../../../models/IGame";
 
 export const gamesApi = createApi({
     reducerPath: 'games',
-    baseQuery: fetchBaseQuery({baseUrl: `https://okhrim-social.onrender.com//api/`}),
+    baseQuery: fetchBaseQuery({baseUrl: `https://okhrim-social.onrender.com/api/`}),
     endpoints: (build) => ({
         getGames: build.query<IGame[], string>({
             query: (keyword: string = '', genres: string = '') => `games?keyword=${keyword}`
