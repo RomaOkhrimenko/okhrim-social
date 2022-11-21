7cd89d8014614d26cf1589c3c2b5b4977eb57642
RomaOkhrimenko <roma.okhrimenko@gmail.com>
2022-11-21 22:09:51 +0200

    try 5

--- front-end/src/store/redux/api/settingsAccountApi.ts
+++ front-end/src/store/redux/api/settingsAccountApi.ts
@@ -2,7 +2,7 @@ import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 
 export const settingsAccountApi = createApi({
     reducerPath: 'settings-account',
-    baseQuery: fetchBaseQuery({baseUrl: `https://okhrim-social.onrender.com/api/settings-account`}),
+    baseQuery: fetchBaseQuery({baseUrl: `https://okhrim-social.onrender.com/api/`}),
     endpoints: (build) => ({
         getSettingsAccount: build.query({
             query: () => `settings-account`
